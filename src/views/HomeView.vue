98b2e15e29130c4d8ee1a8b405c4387c857842d7
nyannss <80017838+nyannss@users.noreply.github.com>
2023-08-31 19:57:29 +0700

    Add vue router, vuex, tailwindcss, and axios

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center text-xl font-medium">
+    Hello world!
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+}
+</script>
